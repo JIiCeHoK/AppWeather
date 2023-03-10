574fc5e4b84a9af4c9b4eb7453981deb646c188d
JIiCeHoK <foxs29546@gmail.com>
2023-03-10 16:51:57 +0500

    Edit: добавил стилей компонентам

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -15,17 +15,19 @@ const mockData = [
 export class SideMenuComponent implements OnInit {
     @Output() selectedCity = new EventEmitter<string>();
 
-    cities = mockData
+    cities = mockData;
+    currentCity = mockData[0];
 
     constructor() {
     }
 
     ngOnInit(): void {
-        this.selectedCity.emit(this.cities[0])
+        this.selectedCity.emit(this.currentCity)
     }
 
     onClick(city: string) {
-        this.selectedCity.emit(city)
+        this.currentCity = city;
+        this.selectedCity.emit(this.currentCity)
     }
 
 }
